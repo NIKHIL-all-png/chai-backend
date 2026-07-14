7b78164f2e4d79113cca8ed1038a8e2aa0ff10a8
NIKHIL-all-png <nkgt9170413117@gmail.com>
2026-07-14 21:47:19 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
+
 // dotenv.config({
 //     path: "./.env"
 // });
 
-console.log("PORT =", process.env.PORT);
-console.log(process.env.MONGODB_URI);
 
-connectDB();
+// console.log("PORT =", process.env.PORT);
+// console.log(process.env.MONGODB_URI);
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB db connection failed !!!",err);
+})
+
+
+
 
 
 
